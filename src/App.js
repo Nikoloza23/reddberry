3efa68fd9660997804a9357cb7451f2d630f0fec
Nikoloza23 <abashia.nikoloza@gmail.com>
2022-02-05 12:35:48 +0400

    react

--- src/App.js
+++ src/App.js
@@ -1,31 +1,11 @@
-//Router 
-import {BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom'
-//Styles 
-import './Style.css'
-//Main  
-import Main from './Main';
-//Routing 
-import Example from './Example';
+import React from 'react'
+import './App.css'
 
-import Main from './Pages/Main';
-
-//Es aris router
-//romelic mushaobs rogorc id aseve clikzec
 function App() {
- 
-  return (
-    <Router>
-    <div className='App'>
-        <Link to='/'></Link>
-        <Link to='/example'></Link>
-      <Routes>
-       <Route path='/' element={<Main />} /> 
-       <Route path='/users/:id' element={<Example />} /> 
-       
-      </Routes>
+   return (
+    <div className="App">
+     
     </div>
-    </Router>
-
   );
 }
 
